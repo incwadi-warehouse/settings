2e293b1ade67455e2447e7353ee5c6ff249b4448
André Baldeweg <a@baldeweg.org>
2022-06-05 22:23:01 +0200

    theming

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import i18n from './i18n.js'
 import { registerSW } from 'virtual:pwa-register'
 import '@baldeweg/ui/styles'
 
+const theme = () => {
+  if (import.meta.env.VUE_APP_THEME !== 'false') {
+    return '<style>@import "' + import.meta.env.VUE_APP_THEME + '";</style>'
+  }
+
+  return ''
+}
+
+document.documentElement.innerHTML += theme()
+
 const ui = createUi()
 const app = createApp(App)
 
